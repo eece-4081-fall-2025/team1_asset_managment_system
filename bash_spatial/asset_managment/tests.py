89ddf55a9a2fd98a13a8e5b9e6a6b7092d543f06
braxton-quillin <bwq04@hotmail.com>
2025-11-12 14:57:19 -0600

    add basic unit test

--- bash_spatial/asset_managment/tests.py
+++ bash_spatial/asset_managment/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from asset_managment.models import Asset, Attribute
 
-# Create your tests here.
+
+class TaskTests(TestCase):
+    def setUp(self):
+        Asset.objects.create(name="Example1")
+
+    def test_task_creation(self):
+        example1 = Asset.objects.get(name="Example1")
+        self.assertEqual(example1.name, "Example1")
