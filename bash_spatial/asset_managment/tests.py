f74508e8529bad29c3739a7e238a27ee155ac7f5
braxton-quillin <bwq04@hotmail.com>
2025-11-12 14:58:00 -0600

    basic unit test

--- bash_spatial/asset_managment/tests.py
+++ bash_spatial/asset_managment/tests.py
@@ -1,11 +1,12 @@
 from django.test import TestCase
-from asset_managment.models import Asset, Attribute
+from asset_managment.models import Asset
 
 
-class TaskTests(TestCase):
+class AssetTests(TestCase):
     def setUp(self):
         Asset.objects.create(name="Example1")
 
-    def test_task_creation(self):
+    # unit test added by Braxton
+    def test_asset_creation(self):
         example1 = Asset.objects.get(name="Example1")
         self.assertEqual(example1.name, "Example1")
